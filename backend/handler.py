aa609fd9b06888b6732fb3692f22c5549107adbd
Yashoza19 <yoyashoza@gmail.com>
2020-06-23 15:37:16 -0400

    Fixing Python Requirements for serverless

--- backend/handler.py
+++ backend/handler.py
@@ -1,25 +1,25 @@
 import json
 import logging
+import boto3
+import time
 
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
+
+dynamodb = boto3.resource("dynamodb")
 
 
 def ping(event, context):
 
     logger.info("Ping requested.")
+
+    table = dynamodb.Table("serverless-chat_Messages")
+    timestamp = int(time.time())
+    table.put_item(Item={"Room":"general","Index": 0, "Timestamp":timestamp, "Username":"ping-user","Content":"PING!"})
+    logger.info("Item added to the database-Messages")
     response = {
         "statusCode": 200,
         "body": "PONG!"
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
