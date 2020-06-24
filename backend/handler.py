5b8a32d559cd2d40ab91942a4d8d650c4bcccf99
Yashoza19 <yoyashoza@gmail.com>
2020-06-23 20:59:40 -0400

    Adding WebSocket functions

--- backend/handler.py
+++ backend/handler.py
@@ -9,49 +9,90 @@ logger.setLevel(logging.DEBUG)
 dynamodb = boto3.resource("dynamodb")
 
 
-def ping(event, context):
-
-    logger.info("Ping requested.")
-
-    table = dynamodb.Table("serverless-chat_Messages")
-    timestamp = int(time.time())
-    table.put_item(Item={"Room":"general","Index": 0, "Timestamp":timestamp, "Username":"ping-user","Content":"PING!"})
-    logger.info("Item added to the database-Messages")
-    response = {
-        "statusCode": 200,
-        "body": "PONG!"
-    }
-
-    return response
-
-def _get_response(status_code, body):
-    if not isinstance(body, str):
-        body = json.dumps(body)
-    return {"statusCode": status_code, "body": body}
-
-def connection_manager(event, context):
-    connectionID = event["requestContext"].get("connectionId")
-    
-    if event["requestContext"]["eventType"] == "CONNECT":
-        logger.info("Connect requested")
-        table = dynamodb.Table("serverless-chat_Connections")
-        table.put_item(Item={"ConnectionID": connectionID})
-        
-    elif event["requestContext"]["eventType"] == "DISCONNECT":
-        logger.info("Disconnect requested")
-        table = dynamodb.Table("serverless-chat_Connections")
-        table.delete_item(Key={"ConnectionID": connectionID})
-        
-    else:
-        logger.error("Connection manager received unrecognized eventType.")
-        return _get_response(500, "Unrecognized eventType.")
-
 def _get_body(event):
     try:
         return json.loads(event.get("body", ""))
     except:
         logger.debug("event body could not be JSON decoded.")
         return {}
+
+
+def _get_response(status_code, body):
+    if not isinstance(body, str):
+        body = json.dumps(body)
+    return {"statusCode": status_code, "body": body}
+
+
+def _send_to_connection(connection_id, data, event):
+    gatewayapi = boto3.client("apigatewaymanagementapi",
+            endpoint_url = "https://" + event["requestContext"]["domainName"] +
+                    "/" + event["requestContext"]["stage"])
+    return gatewayapi.post_to_connection(ConnectionId=connection_id,
+            Data=json.dumps(data).encode('utf-8'))
+
+def connection_manager(event, context):
+    connectionID = event["requestContext"].get("connectionId")
+    
+    if event["requestContext"]["eventType"] == "CONNECT":
+        logger.info("Connect requested")
+
+        # Add connectionID to the database
+        table = dynamodb.Table("serverless-chat_Connections")
+        table.put_item(Item={"ConnectionID": connectionID})
+        return _get_response(200, "Connect successful.")
+
+    elif event["requestContext"]["eventType"] == "DISCONNECT":
+        logger.info("Disconnect requested")
+
+        # Remove the connectionID from the database
+        table = dynamodb.Table("serverless-chat_Connections")
+        table.delete_item(Key={"ConnectionID": connectionID})
+        return _get_response(200, "Disconnect successful.")
+
+    else:
+        logger.error("Connection manager received unrecognized eventType '{}'")
+        return _get_response(500, "Unrecognized eventType.")
+
+
+def default_message(event, context):
+    """
+    Send back error when unrecognized WebSocket action is received.
+    """
+    logger.info("Unrecognized WebSocket action received.")
+    return _get_response(400, "Unrecognized WebSocket action.")
+
+
+def get_recent_messages(event, context):
+    """
+    Return the 10 most recent chat messages.
+    """
+    connectionID = event["requestContext"].get("connectionId")
+    logger.info("Retrieving most recent messages for CID '{}'"\
+            .format(connectionID))
+
+    # Ensure connectionID is set
+    if not connectionID:
+        logger.error("Failed: connectionId value not set.")
+        return _get_response(500, "connectionId value not set.")
+
+    # Get the 10 most recent chat messages
+    table = dynamodb.Table("serverless-chat_Messages")
+    response = table.query(KeyConditionExpression="Room = :room",
+            ExpressionAttributeValues={":room": "general"},
+            Limit=10, ScanIndexForward=False)
+    items = response.get("Items", [])
+
+    # Extract the relevant data and order chronologically
+    messages = [{"username": x["Username"], "content": x["Content"]}
+            for x in items]
+    messages.reverse()
+
+    # Send them to the client who asked for it
+    data = {"messages": messages}
+    _send_to_connection(connectionID, data, event)
+
+    return _get_response(200, "Sent recent messages to '{}'."\
+            .format(connectionID))
 
 
 def send_message(event, context):
@@ -63,57 +104,39 @@ def send_message(event, context):
                     .format(attribute))
             return _get_response(400, "'{}' not in message dict"\
                     .format(attribute))
-
+    table = dynamodb.Table("serverless-chat_Messages")
     response = table.query(KeyConditionExpression="Room = :room",
             ExpressionAttributeValues={":room": "general"},
             Limit=1, ScanIndexForward=False)
     items = response.get("Items", [])
     index = items[0]["Index"] + 1 if len(items) > 0 else 0
+
+    # Add the new message to the database
     timestamp = int(time.time())
     username = body["username"]
     content = body["content"]
     table.put_item(Item={"Room": "general", "Index": index,
             "Timestamp": timestamp, "Username": username,
             "Content": content})
-    
+
+    # Get all current connections
     table = dynamodb.Table("serverless-chat_Connections")
     response = table.scan(ProjectionExpression="ConnectionID")
     items = response.get("Items", [])
     connections = [x["ConnectionID"] for x in items if "ConnectionID" in x]
 
+    # Send the message data to all connections
     message = {"username": username, "content": content}
     logger.debug("Broadcasting message: {}".format(message))
     data = {"messages": [message]}
     for connectionID in connections:
         _send_to_connection(connectionID, data, event)
+    return _get_response(200, "Message sent to {} connections."\
+            .format(len(connections)))
 
-    return _get_response(200, "Message sent to all connections.")
-
-def _send_to_connection(connection_id, data, event):
-    gatewayapi = boto3.client("apigatewaymanagementapi",
-            endpoint_url = "https://" + event["requestContext"]["domainName"] +
-                    "/" + event["requestContext"]["stage"])
-    return gatewayapi.post_to_connection(ConnectionId=connection_id,
-            Data=json.dumps(data).encode('utf-8'))
-
-def get_recent_messages(event, context):
+def ping(event, context):
     """
-    Returns 10 most recent chat messages
+    Sanity check endpoint that echoes back 'PONG' to the sender.
     """
-    logger.info("Retrieving most recent messages.")
-    connectionID = event["requestContext"].get("connectionId")
-
-    table = dynamodb.Table("serverless-chat_Messages")
-    response = table.query(KeyConditionExpression="Room = :room",
-            ExpressionAttributeValues={":room": "general"},
-            Limit=10, ScanIndexForward=False)
-    items = response.get("Items", [])
-
-    messages = [{"username": x["Username"], "content": x["Content"]}
-            for x in items]
-    messages.reverse()
-
-    data = {"messages": messages}
-    _send_to_connection(connectionID, data, event)
-
-    return _get_response(200, "Sent recent messages.")
+    logger.info("Ping requested.")
+    return _get_response(200, "PONG!")
